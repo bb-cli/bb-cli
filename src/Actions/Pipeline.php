8204bf82aefac4b89ea95edb8646540df33e0480
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2021-08-17 15:01:47 +0300

    pipeline methods updated

--- src/Actions/Pipeline.php
+++ src/Actions/Pipeline.php
@@ -9,18 +9,13 @@ class Pipeline extends Base
     public const DEFAULT_METHOD = 'get';
 
     public const AVAILABLE_COMMANDS = [
-        'get' => 'get, latest',
+        'get' => 'get',
+        'latest' => 'latest',
     ];
 
-    public function get($pipeLineNumber = '')
+    public function get($pipeLineNumber)
     {
         $response = $this->makeRequest('GET', "/pipelines/{$pipeLineNumber}");
-
-        if (empty($pipeLineNumber)) {
-            // if no pipeLineNumber is given, get last pipeLine
-            $pipeLineNumber = $response['size'];
-            $response = $this->makeRequest('GET', "/pipelines/{$pipeLineNumber}");
-        }
 
         $repoPath = getRepoPath();
         e(
@@ -35,7 +30,14 @@ class Pipeline extends Base
                 'completed' => array_get($response, 'completed_on'),
                 'link' => "https://bitbucket.org/$repoPath/addon/pipelines/home#!/results/$pipeLineNumber",
             ],
-            'blue'
+            'yellow'
         );
+    }
+
+    public function latest()
+    {
+        $response = $this->makeRequest('GET', '/pipelines/');
+
+        return $this->get($response['size']);
     }
 }
