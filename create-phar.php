24e06f103c36c337f92ae0ab100335efcfbb1fc8
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2021-08-20 09:17:40 +0300

    phar builder script updated

--- create-phar.php
+++ create-phar.php
@@ -1,6 +1,19 @@
 <?php
 
+$pharIndexFile = 'phar-index.php';
+
+$binFileContents = file_get_contents('bin/bb');
+file_put_contents(
+    $pharIndexFile,
+    str_replace(
+        ['/../src/', "#!/usr/bin/env php\n"],
+        ['/src/', ''],
+        $binFileContents
+    )
+);
+
 $phar = new Phar('bb.phar');
-$phar->buildFromDirectory(dirname(__FILE__) . '/src');
+$phar->buildFromDirectory(dirname(__FILE__), '[src|config|phar\-index\.php]');
 $phar->setStub("#!/usr/bin/env php\n".$phar->createDefaultStub('phar-index.php'));
-// $phar->compress(PHAR::GZ);
+
+unlink($pharIndexFile);
